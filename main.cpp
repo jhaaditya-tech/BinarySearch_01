cac9c030e5785ea52958a703298335b29c594e0f
Aditya Jha <110705897+jhaaditya-tech@users.noreply.github.com>
2022-12-16 06:19:06 -0500

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  BinarySearch
+//
+//  Created by Aditya Jha on 12/16/22.
+//
+
+#include <iostream>
+using namespace std;
+int main(){
+    int A[10]={6,8,13,17,20,22,25,28,30,35};
+    int l=0,h=9,key,mid;
+    cout<<"Enter key: ";
+    cin>>key;
+    while (l<=h){
+        mid=(l+h)/2;
+        if (key==A[mid]){
+            cout<<mid<<endl;
+            return 0;
+        }
+        else if (key<mid){
+            h=mid-1;
+        }
+        else{
+            l=mid+1;
+        }
+    }
+    cout<<"Search Failes. Couldn't locate data"<<endl;
+    return 0;
+}
